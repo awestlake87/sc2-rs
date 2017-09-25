772286c0167f6795da83a55204489d244da50110
Andrew J Westlake <kelixes@gmail.com>
2017-09-25 04:37:42 -0500

    generated sc2 protobuf code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+
+extern crate protobuf;
+
+mod proto;
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/proto/mod.rs
@@ -0,0 +1,11 @@
+
+pub mod common;
+pub mod data;
+pub mod debug;
+pub mod error;
+pub mod query;
+pub mod raw;
+pub mod sc2api;
+pub mod score;
+pub mod spatial;
+pub mod ui;
