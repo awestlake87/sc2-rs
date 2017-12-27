0ca159957508d3c1d13fca6c83bbb7ca3fb21d3c
Andrew J Westlake <kelixes@gmail.com>
2017-12-27 14:50:18 -0600

    converted ClientLobe into a state machine lobe

--- src/lobes/client.rs
+++ src/lobes/client.rs
@@ -15,59 +15,134 @@ use super::{ Message, Soma, Role };
 
 const NUM_RETRIES: u32 = 10;
 
-pub struct ClientLobe {
-    soma:               Soma,
+pub enum ClientLobe {
+    Init(ClientInit),
+    AwaitInstance(ClientAwaitInstance),
+    Connect(ClientConnect),
 
-    instance:           Option<Uuid>,
-
-    timer:              Timer,
-    retries:            u32,
+    GameSetup(ClientGameSetup),
 }
 
 impl ClientLobe {
     pub fn new() -> Result<Self> {
         Ok(
-            Self {
-                soma: Soma::new(
-                    vec![
-                        Constraint::RequireOne(Role::InstanceProvider),
-                        Constraint::RequireOne(Role::Client)
-                    ],
-                    vec![ ]
-                )?,
-
-                instance: None,
-
-                timer: Timer::default(),
-                retries: 0,
-            }
+            ClientLobe::Init(
+                ClientInit {
+                    soma: Soma::new(
+                        vec![
+                            Constraint::RequireOne(Role::InstanceProvider),
+                            Constraint::RequireOne(Role::Client)
+                        ],
+                        vec![ ]
+                    )?,
+                }
+            )
         )
     }
+}
 
-    fn assign_instance(
-        mut self, src: Handle, instance: Uuid, url: Url
-    )
-        -> Result<Self>
+impl Lobe for ClientLobe {
+    type Message = Message;
+    type Role = Role;
+
+    fn update(self, msg: Protocol<Message, Role>) -> cortical::Result<Self> {
+        match self {
+            ClientLobe::Init(state) => state.update(msg),
+            ClientLobe::AwaitInstance(state) => state.update(msg),
+            ClientLobe::Connect(state) => state.update(msg),
+            ClientLobe::GameSetup(state) => state.update(msg),
+        }.chain_err(
+            || cortical::ErrorKind::LobeError
+        )
+    }
+}
+
+pub struct ClientInit {
+    soma:               Soma
+}
+
+impl ClientInit {
+    fn update(mut self, msg: Protocol<Message, Role>) -> Result<ClientLobe> {
+        self.soma.update(&msg)?;
+
+        match msg {
+            Protocol::Start => self.start(),
+
+            _ => Ok(ClientLobe::Init(self))
+        }
+    }
+
+    fn start(self) -> Result<ClientLobe> {
+        Ok(ClientLobe::AwaitInstance(ClientAwaitInstance { soma: self.soma }))
+    }
+}
+
+pub struct ClientAwaitInstance {
+    soma:               Soma,
+}
+
+impl ClientAwaitInstance {
+    fn update(mut self, msg: Protocol<Message, Role>) -> Result<ClientLobe> {
+        self.soma.update(&msg)?;
+
+        match msg {
+            Protocol::Message(
+                src, Message::ProvideInstance(instance, url)
+            ) => {
+                self.assign_instance(src, instance, url)
+            },
+
+            _ => Ok(ClientLobe::AwaitInstance(self))
+        }
+    }
+
+    fn assign_instance(self, src: Handle, _: Uuid, url: Url)
+        -> Result<ClientLobe>
     {
         assert_eq!(src, self.soma.req_input(Role::InstanceProvider)?);
 
-        if self.instance.is_none() {
-            self.instance = Some(instance);
-            self.retries = NUM_RETRIES;
+        let this_lobe = self.soma.effector()?.this_lobe();
+        self.soma.effector()?.send(
+            this_lobe, Message::AttemptConnect(url)
+        );
 
-            let this_lobe = self.soma.effector()?.this_lobe();
-            self.soma.effector()?.send(
-                this_lobe, Message::AttemptConnect(url)
-            );
+        Ok(
+            ClientLobe::Connect(
+                ClientConnect {
+                    soma: self.soma,
 
-            Ok(self)
-        }
-        else {
-            bail!("instance has already been assigned")
+                    timer: Timer::default(),
+                    retries: NUM_RETRIES,
+                }
+            )
+        )
+    }
+}
+
+pub struct ClientConnect {
+    soma:               Soma,
+
+    timer:              Timer,
+    retries:            u32,
+}
+
+impl ClientConnect {
+    fn update(mut self, msg: Protocol<Message, Role>) -> Result<ClientLobe> {
+        self.soma.update(&msg)?;
+
+        match msg {
+            Protocol::Message(src, Message::AttemptConnect(url)) => {
+                self.attempt_connect(src, url)
+            },
+            Protocol::Message(src, Message::Connected) => {
+                self.on_connected(src)
+            },
+
+            _ => Ok(ClientLobe::Connect(self))
         }
     }
 
-    fn attempt_connect(mut self, src: Handle, url: Url) -> Result<Self> {
+    fn attempt_connect(mut self, src: Handle, url: Url) -> Result<ClientLobe> {
         assert_eq!(src, self.soma.effector()?.this_lobe());
 
         let connected_effector = self.soma.effector()?.clone();
@@ -75,8 +150,6 @@ impl ClientLobe {
         let timer_effector = self.soma.effector()?.clone();
 
         let client_remote = self.soma.effector()?.remote();
-
-        let owner = self.soma.req_input(Role::Client)?;
 
         if self.retries == 0 {
             bail!("unable to connect to instance")
@@ -99,8 +172,9 @@ impl ClientLobe {
                     move |_| connect_async(url, client_remote)
                         .and_then(
                             move |(_ws_stream, _)| {
+                                let this_lobe = connected_effector.this_lobe();
                                 connected_effector.send(
-                                    owner, Message::Connected
+                                    this_lobe, Message::Connected
                                 );
 
                                 Ok(())
@@ -131,32 +205,28 @@ impl ClientLobe {
                 )
         );
 
-        Ok(self)
+        Ok(ClientLobe::Connect(self))
+    }
+
+    fn on_connected(self, src: Handle) -> Result<ClientLobe> {
+        assert_eq!(src, self.soma.effector()?.this_lobe());
+
+        self.soma.send_req_input(Role::Client, Message::Connected)?;
+
+        Ok(ClientLobe::GameSetup(ClientGameSetup { soma: self.soma }))
     }
 }
 
-impl Lobe for ClientLobe {
-    type Message = Message;
-    type Role = Role;
+pub struct ClientGameSetup {
+    soma:           Soma,
+}
 
-    fn update(mut self, msg: Protocol<Message, Role>)
-        -> cortical::Result<Self>
-    {
+impl ClientGameSetup {
+    fn update(mut self, msg: Protocol<Message, Role>) -> Result<ClientLobe> {
         self.soma.update(&msg)?;
 
         match msg {
-            Protocol::Message(
-                src, Message::ProvideInstance(instance, url)
-            ) => {
-                self.assign_instance(src, instance, url)
-            },
-            Protocol::Message(src, Message::AttemptConnect(url)) => {
-                self.attempt_connect(src, url)
-            },
-
-            _ => Ok(self),
-        }.chain_err(
-            || cortical::ErrorKind::LobeError
-        )
+            _ => Ok(ClientLobe::GameSetup(self))
+        }
     }
 }
