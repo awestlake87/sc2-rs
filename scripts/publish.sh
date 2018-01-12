d79c1401779cd1a996e1b284087964e5ec92257a
Andrew J Westlake <kelixes@gmail.com>
2018-01-12 00:04:18 -0600

    trying to fix the issue

--- scripts/publish.sh
+++
@@ -1,4 +0,0 @@
-travis-cargo --only stable publish -- --token $CARGO_TOKEN &&
-export VERSION=`cargo pkgid | sed -E 's/.*#(.*:)?(.+)/\2/'` &&
-git tag v$VERSION &&
-git push https://awestlake87:$GITHUB_TOKEN@github.com/awestlake87/sc2-rs v$VERSION
